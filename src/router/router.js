7efbf705a51f095942fcdc6bd8f43477d1c19199
ishraqe <kanak431@diu.edu.bd>
2018-03-14 00:24:35 +0600

    animation in carousol not done yet

--- src/router/router.js
+++ src/router/router.js
@@ -26,7 +26,7 @@ class RouterComponent extends Component {
                             component={Initial}
                         />
                         <Scene
-                            initial
+                            
                             key='auth_screen'
                             hideNavBar={true}
                             component={Auth}
--- src/screens/InitialScreen.js
+++ src/screens/InitialScreen.js
@@ -2,16 +2,42 @@ import React, {Component} from 'react';
 import {View, Text, Image, Animated} from 'react-native';
 
 import styles from '../styles/AuthScreenStyle';
+import { Actions } from 'react-native-router-flux';
 
 
 class Initial extends Component {
+
+    state = {
+        animation : new Animated.Value(-300),
+    }
+
+    componentDidMount() {
+        Animated.timing(this.state.animation, {
+            toValue: 0,
+            duration: 1500
+        }).start(() =>  {
+            setTimeout(() => {
+                Actions.auth_screen();
+            }, 1000);
+        });
+       
+    }
+
     render () {
+        const animatedStyle = {
+            transform: [
+                { 
+                  translateX: this.state.animation
+                }
+            ]
+        };      
+
         return (
             <View style={styles.container}>
                 <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-                    <Image
+                    <Animated.Image
                         source={require('../assets/icon.png')}
-                        style={{height: 100, width: 100}} />
+                        style={[{height: 100, width: 100}, animatedStyle]} />
                     <Text style={styles.logoHeader}>Moving Company</Text>
                 </View>
             </View>
